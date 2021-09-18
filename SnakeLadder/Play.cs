59832c3d9b64113e74aac5601a808b43ea8bb872
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-18 15:47:37 +0530

    [Chaitanya] Add: Initialized player position with start position

---
+++ SnakeLadder/Play.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    class Play
+    {
+        // initialize constants
+        const int START_POSITION = 0, END_POSITION = 100;
+
+        // initialize variables
+        int playerPosition = START_POSITION;
+
+        //Console.WriteLine($"player position is: {playerPosition}");
+    }
+}
--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -6,7 +6,7 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!\nWelcome to snake ladder game.");
+
         }
     }
 }
