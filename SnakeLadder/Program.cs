7338cd472bf9d1cd92afa9edc84c7b67776f8d2b
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-18 14:10:44 +0530

    [Chaitanya] Add: Added folder Structure and welcome message

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\nWelcome to snake ladder game.");
+        }
+    }
+}
